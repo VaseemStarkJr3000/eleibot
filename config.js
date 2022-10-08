4c94a6a66d36a21bbe933e4be764650220c336ae
Ronen <101617329+Ronen6999@users.noreply.github.com>
2022-10-08 13:36:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ global.OwnerNumber = ['15069751205']
 global.ownertag = ['15069751205']
 global.BotName = "Elei🐧"
 global.packname = "Elei bot🐧"
-global.author = "By: Ronen💜"
+global.author = "By : Ronen💜"
 global.OwnerName = "Ronen💜"
 global.BotSourceCode = "https://github.com/Ronen6999/eleibot"
 global.SupportGroupLink = "https://chat.whatsapp.com/Hooxu1kZEoFKoNpdFLqXMO"
@@ -61,13 +61,13 @@ global.mess = {
     jobdone: 'Job done...',
     useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
     botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
-    botowner: 'Only my *Owner* can use this command, Baka!',
-    grouponly: 'This command is only made for *Groups*, Baka!',
-    privateonly: 'This command is only made for *Private Chat*, Baka!',
+    botowner: 'Only my *Owner* can use this command!',
+    grouponly: 'This command is only made for *Groups*!',
+    privateonly: 'This command is only made for *Private Chat!*',
     botonly: 'Only the *Bot itself* can use this command!',
-    waiting: 'Chotto Matte...',
-    nolink: 'Please provide me *link*, Baka!',
-    error: 'An error occurd!',
+    waiting: 'plz wait a min...',
+    nolink: 'Kidly provide me the *link*!',
+    error: 'An error occurred!',
     banned: 'You are *Banned* fron using commands!',
     bangc: 'This Group is *Banned* from using Commands!',
     nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
